451ef88dd12bbe58d36f16e3f41ea9e59e8a7cf2
Mark Laskey <laskey.mark@hotmail.com>
2018-11-01 08:43:58 -0400

    started profile reducer

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
 
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
@@ -94,5 +96,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
